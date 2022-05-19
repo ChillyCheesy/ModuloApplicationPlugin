af2965f136226a66e0f1e9484d4fd79c7c72732d
AymericHenouille <aymeric.henouille@gmail.com>
2022-05-19 23:48:26 +0200

    feat: download method

--- src/main/groovy/com/chillycheesy/modulo/tasks/RunModuloServer.groovy
+++ src/main/groovy/com/chillycheesy/modulo/tasks/RunModuloServer.groovy
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 
 class RunModuloServer implements ModuloTask {
 
+    private final def LINK = 'https://s01.oss.sonatype.org/content/repositories/releases/com/chillycheesy/modulo-server/'
+
     private ModuleExtension moduleExtension
 
     RunModuloServer(ModuleExtension moduleExtension) {
@@ -17,7 +19,25 @@ class RunModuloServer implements ModuloTask {
             group = 'modulo'
             description = 'Run the Modulo server.'
             doLast {
+                final File file = new File("modulo/modulo-server-${moduleExtension.version}.jar")
+                if (!file.exists()) downloadFile(file)
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+            }
+        }
+    }
 
+    private def downloadFile(File file) {
+        def url = "$LINK:${moduleExtension.version}/${file.getName()}"
+        new URL(url).openConnection().with { connection ->
+            connection.instanceFollowRedirects = false
+            url = connection.getHeaderField("Location")
+            if(!url) {
+                file.withOutputStream { outputStream ->
+                    connection.inputStream.with { inputStream ->
+                        outputStream << inputStream
+                        inputStream.close()
+                    }
+                }
             }
         }
     }
